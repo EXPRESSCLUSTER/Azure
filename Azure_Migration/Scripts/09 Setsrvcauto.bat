8e24940ac64a32d363ee57f3a62adeb31600ccfc
Miyamoto Kazuyuki <kazuyuki@nec.com>
2026-08-05 09:19:46 +0900

    Update Setsrvcauto.bat to enable automatic startup
    Added script to set EXPRESSCLUSTER services to automatic startup.

--- Azure_Migration/Scripts/09 Setsrvcauto.bat
+++ Azure_Migration/Scripts/09 Setsrvcauto.bat
@@ -1,1 +1,6 @@
+@echo off
+rem ==================================================================
+rem =Set all EXPRESSCLUSTER services from Manual to Automatic-startup=
+rem ==================================================================
+
 call clpsvcctrl.bat --enable -a
